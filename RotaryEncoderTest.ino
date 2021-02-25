68ce850dd8281f85b1cd30af58e1850e00325fbb
ThomasBruflot <60750739+ThomasBruflot@users.noreply.github.com>
2021-02-25 11:14:18 +0100

    Måte å bruke rotary encoder på

---
+++ RotaryEncoderTest.ino
@@ -0,0 +1,32 @@
+#define encoder0PinA  2
+#define encoder0PinB  4
+
+volatile unsigned int encoder0Pos = 0;
+
+void setup() {
+  pinMode(encoder0PinA, INPUT);
+  digitalWrite(encoder0PinA, HIGH);       // turn on pull-up resistor
+  pinMode(encoder0PinB, INPUT);
+  digitalWrite(encoder0PinB, HIGH);       // turn on pull-up resistor
+
+  attachInterrupt(0, doEncoder, CHANGE);  // encoder pin on interrupt 0 - pin 2
+  Serial.begin (9600);
+  Serial.println("start");                // a personal quirk
+}
+
+void loop() {
+  // do some stuff here - the joy of interrupts is that they take care of themselves
+}
+
+void doEncoder() {
+  /* If pinA and pinB are both high or both low, it is spinning
+     forward. If they're different, it's going backward.
+  */
+  if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)) {
+    encoder0Pos++;
+  } else {
+    encoder0Pos--;
+  }
+
+  Serial.println (encoder0Pos, DEC);
+}
