9f813e8c1af57dc8f03b3ff11ea1256eb39bfd0a
ThomasBruflot <60750739+ThomasBruflot@users.noreply.github.com>
2021-03-02 12:44:14 +0100

    Update RotaryEncoderTest.ino

--- Arduino/RotaryEncoderTest.ino
+++ Arduino/RotaryEncoderTest.ino
@@ -1,24 +1,45 @@
-#define encoder0PinA  2
-#define encoder0PinB  4
+/*
+Forklaring til pins:
 
-volatile unsigned int encoder0Pos = 0;
+GND - ground-pin
++ - 5V-pin
+SW - button-pin (encoder0pinC i denne koden)
+
+For rotasjon:
+DT - Output A
+CLK - OutputB
+*/
+
+const int encoder0PinA = 2;
+const int encoder0PinB = 4;
+const int encoder0PinC = 7;//Button
+unsigned long lastFire = 0;
+
+volatile int encoder0Pos = 0;
+
 
 void setup() {
-  pinMode(encoder0PinA, INPUT);
-  digitalWrite(encoder0PinA, HIGH);       // turn on pull-up resistor
-  pinMode(encoder0PinB, INPUT);
-  digitalWrite(encoder0PinB, HIGH);       // turn on pull-up resistor
-
-  attachInterrupt(0, doEncoder, CHANGE);  // encoder pin on interrupt 0 - pin 2
+  pinMode(encoder0PinC, INPUT_PULLUP);
+  
+  pinMode(encoder0PinA, INPUT_PULLUP);       // turn on pull-up resistor
+  pinMode(encoder0PinB, INPUT_PULLUP);       // turn on pull-up resistor
+  //pinMode(interruptPinC, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoder, CHANGE);  // encoder pin on interrupt 0 - pin 2
+  
   Serial.begin (9600);
   Serial.println("start");                // a personal quirk
 }
 
 void loop() {
   // do some stuff here - the joy of interrupts is that they take care of themselves
+  if(digitalRead(encoder0PinC) == LOW) {
+    Serial.println("Knappen er trykket - loop");
+  }
 }
 
+
 void doEncoder() {
+  
   /* If pinA and pinB are both high or both low, it is spinning
      forward. If they're different, it's going backward.
   */
