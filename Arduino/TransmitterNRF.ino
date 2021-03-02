769b702c6c773dec6da352d621c5d97d09769c99
rosemamb <55994796+rosemamb@users.noreply.github.com>
2021-03-02 15:43:30 +0100

    Add files via upload

--- Arduino/TransmitterNRF.ino
+++ Arduino/TransmitterNRF.ino
@@ -10,16 +10,23 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#define button 2
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
+boolean buttonState = 0;
 void setup() {
+  pinMode(button, INPUT_PULLUP);
+  Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  
 }
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+  //const char text[] = "Hello World";
+ // Serial.println(text);
+  radio.stopListening();
+  //radio.write(&text, sizeof(text));
+  buttonState = digitalRead(button);
+  radio.write(&buttonState, sizeof(buttonState));
 }
