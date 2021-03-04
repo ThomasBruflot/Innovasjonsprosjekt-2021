d5539c14aee223de90862d3210818b1017a8c42e
ThomasBruflot <60750739+ThomasBruflot@users.noreply.github.com>
2021-03-04 11:31:19 +0100

    Update LoadCell.ino

--- Arduino/LoadCell.ino
+++ Arduino/LoadCell.ino
@@ -4,7 +4,7 @@ int sclRead = 0; //Verdien til O+
 int sdaRead = 0;  //Verdien til O-
 int diff = 0; //Differansen mellom spenningen fra O+ og O-
 bool active; //Sier om vi har kraft på sensoren
-int ref = 1; //Referansespenning som man måler utifra, ideelt 0 men her 1
+int ref = 60; //Referansespenning som man måler utifra, ideelt 0 men her 1
 
 void setup() {
   Serial.begin(9600);           //  setup serial
@@ -14,15 +14,15 @@ void setup() {
 void loop() {
   sclRead = analogRead(sclPin);  // read the input pin O-
   sdaRead = analogRead(sdaPin); //read the input pin O+
-  diff = sdaRead - sclRead;  //Regner differansen (virtuell ground)
-  Serial.println(diff);          // debug value
+  diff = sclRead - sdaRead;  //Regner differansen (virtuell ground)
+  //Serial.println(diff);          // debug value
 
-  if((diff+1) > ref) {
+  if(diff > ref) {
     active = HIGH; //Hvis vi har over 1 mer differanse på spenningene 
                    //sier vi at det er merkbar kraft på sensoren 
   }
   else {
     active = LOW; //Hvis vi ikke har en stor nok differanse på spenningen
   }
-  
+  Serial.println(active);  
 }
