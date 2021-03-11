4a74aa496d8c5d1b60d9471d17b5561ebc8a861a
Henrik <56533560+HenrikH99@users.noreply.github.com>
2021-03-11 11:21:21 +0100

    Update RotaryEncoder.ino

--- DisplayTime/RotaryEncoder.ino
+++ DisplayTime/RotaryEncoder.ino
@@ -1,44 +1,72 @@
 //Funksjon for å justere tiden med rotary encoder.
-void encoderState(){
-  REpushCount++;
-  if(REpushCount > 2){
+void encoderState() {
+  if (REpushCount > 3) {
     REpushCount = 0;
   }
+  Serial.println("count: " + String(REpushCount));
+  switch (REpushCount) {
+    case 0:
+      stopTimeForBothTeams();
+      //printText("Count: " + String(REpushCount), 20, 140, 1, ILI9341_WHITE, tft);
+      break;
+    case 1:
+      stopTimeForBothTeams();
+      //printText("Count: " + String(REpushCount), 20, 140, 1, ILI9341_WHITE, tft);
+      break;
+    case 2:
+      stopTimeForBothTeams();
+      //printText("Count: " + String(REpushCount), 20, 140, 1, ILI9341_WHITE, tft);
+      break;
+    default:
+      break;
+  }
+  REpushCount++;
 
-  switch (REpushCount)
-  {
-  case 0:
-    //adjustTime();
-    printText("RTpushCount: " + REpushCount, 0, 140, 3, ILI9341_WHITE, tft);
-    break;
-  case 1:
-    printText("RTpushCount: " + REpushCount, 0, 140, 3, ILI9341_WHITE, tft);
-    break;
-  case 2:
-    printText("RTpushCount: " + REpushCount, 0, 140, 3, ILI9341_WHITE, tft);
-    break;
-  default:
-    break;
-  }      
-} 
+}
 
-/*void chooseTeamAorB(){
-    if(encoderPosChoose < 0) {
-      timeA += encoderPos;
-      timeAdjustedA = encoderPos;
-     } 
-     else if (encoderPosChoose > 0) {
-        timeB += encoderPos;
-        timeAdjustedB = encoderPos;
-      }
-}*/
+void chooseTeamAorB(int encoderVal) {
+  if (encoderVal >= 0) {
+    Serial.println("Top display");
+    tft2.fillRect(0, 145, 250, 15, ILI9341_BLACK); //paint a black square over message in display 2
+    printText("Adjust time here: ", 5, 150, 2, ILI9341_RED, tft);
+  } else {
+    Serial.println("Bottom display");
+    tft.fillRect(0, 145, 250, 15, ILI9341_BLACK); //paint a black square over message in display 1
+    printText("Adjust time here: ", 5, 150, 2, ILI9341_RED, tft);
+  }
+}
 
-//Konfigurasjon ved oppstart for å sette tiden.
-/*void configuration() {
-  REpushCount = digitalRead(encoderDT);
-  
-  
-    timeA = encoderPos;
-    timeB = encoderPos;
-     
-  }*/
+//Returns a value corresponding to the rotation of the encoder
+void encoderRotation() {
+  switch (REpushCount) {
+    case 0:
+      //printText("Count: " + String(REpushCount), 20, 140, 1, ILI9341_WHITE, tft);
+      break;
+    case 1:
+      chooseTeamAorB(encoderValue);
+      //printText("Count: " + String(REpushCount), 20, 140, 1, ILI9341_WHITE, tft);
+      break;
+    case 2:
+      //printText("Count: " + String(REpushCount), 20, 140, 1, ILI9341_WHITE, tft);
+      break;
+    default:
+      break;
+  }
+
+  if (digitalRead(encoderDT) == digitalRead(encoderCLK)) {
+    encoderValue += 5;
+  } else {
+    encoderValue -= 5;
+  }
+  Serial.println("RE value: " + String(encoderValue));
+}
+
+void stopTimeForBothTeams() {
+  //stop time for both teams
+  digitalWrite(ledA, LOW);
+  startPinAon = false;
+  stopPinAon = true;
+  digitalWrite(ledB,  LOW);
+  startPinBon = false;
+  stopPinBon = true;
+}
