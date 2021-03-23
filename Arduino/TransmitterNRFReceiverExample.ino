400efb024b622e7065472af66f4b8f4700fa17d9
ariaalinejad <79251626+ariaalinejad@users.noreply.github.com>
2021-03-23 15:08:24 +0100

    Add files via upload

---
+++ Arduino/TransmitterNRFReceiverExample.ino
@@ -0,0 +1,55 @@
+
+
+// SimpleRx - the slave or the receiver
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+#define CE_PIN   7
+#define CSN_PIN 8
+
+const byte thisSlaveAddress[6] = "00001";
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+int dataReceived; // this must match dataToSend in the TX
+bool newData = false;
+
+//===========
+
+void setup() {
+
+    Serial.begin(9600);
+
+    Serial.println("SimpleRx Starting");
+    radio.begin();
+    radio.setDataRate( RF24_250KBPS );
+    radio.openReadingPipe(0, thisSlaveAddress);
+    radio.startListening();
+}
+
+//=============
+
+void loop() {
+    getData();
+    showData();
+}
+
+//==============
+
+void getData() {
+    if ( radio.available() ) {
+        radio.read( &dataReceived, sizeof(dataReceived) );
+        newData = true;
+    }
+}
+
+void showData() {
+    if (newData == true) {
+        Serial.print("Data received ");
+        Serial.println(dataReceived);
+        newData = false;
+    }
+}
